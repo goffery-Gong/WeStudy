fa22a81c565903952f9d39bdd0dac370a80b37bd
goffery.gzw <goffery.gzw@alibaba-inc.com>
2019-08-03 16:31:52 +0800

    [add]添加了题目说明

--- src/NetEase/gzw/QuestionOne.java
+++ src/NetEase/gzw/QuestionOne.java
@@ -1,7 +1,6 @@
 package NetEase.gzw;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @Author LingLong.gzw
@@ -9,45 +8,66 @@ import java.util.Scanner;
  */
 public class QuestionOne {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        ArrayList<String> result = new ArrayList<>();
-        Scanner scanner= new Scanner(System.in);
-        int len = scanner.nextInt();
-        int index=0;
-        int maxx = 0;
-        //scanner.nextLine();
-        while(scanner.hasNext()){
-            String s = scanner.next();
-            String[] array = s.split(",");
-            int len1 = array.length;
-            if(maxx<len1)
-                maxx=len1;
-            list.add(s);
-        }
-        scanner.close();
-        while(index < maxx){
-            for(int i=0;i<list.size();i++){
-                String str = list.get(i);
-                String[] array = str.split(",");
-                int maxLen=array.length;
-                int endLen=index+len;
-                if(maxLen < endLen)
-                    endLen = maxLen;
-                for(int j=index;j<endLen;j++){
-                    String out=array[j];
-                    result.add(out);
-                    result.add(",");
-                    //System.out.print(out);
-                    //if(j != maxx-1)
-                    //System.out.print(",");
-                }
-            }
-            index += len;
-        }
-        int rlen = result.size();
-        result.remove(rlen-1);
-        for(String a:result)
-            System.out.print(a);
+        List<Integer> scores = new ArrayList<>();
+        List<Integer> indexs = new ArrayList<>();
 
+        Scanner scanner = new Scanner(System.in);
+        //班级人数
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            scores.add(scanner.nextInt());
+        }
+        //查询人数
+        int q = scanner.nextInt();
+        for (int i = 0; i < q; i++) {
+            indexs.add(scanner.nextInt());
+        }
+
+        List<Integer> newScores = new ArrayList<>(scores);
+        List<Integer> resultList = new ArrayList<>();
+        String result ;
+        Collections.sort(newScores);
+        for (int index : indexs) {
+            double scoreIndex = getScoreIndex(index - 1, scores, newScores);
+            result = String.format("%.6f", scoreIndex * 100 / n);
+            System.out.println(result);
+
+        }
+    }
+
+    /**
+     * @param index     输入的学生index
+     * @param scores
+     * @param newScores
+     * @return
+     * 3
+     * 98 87 100
+     * 3
+     * 1
+     * 2
+     * 3
+     */
+    private static int getScoreIndex(int index, List<Integer> scores, List<Integer> newScores) {
+        //学生分数
+        int target = scores.get(index);
+        int low = 0;
+        int high = newScores.size() - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (target == newScores.get(mid)) {
+                return mid;
+            } else if (target < newScores.get(mid)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        for (int i=mid+1; i<high;i++){
+            if(newScores.get(i).equals(newScores.get(mid))){
+                mid++;
+            }
+        }
+        return -1;
     }
 }
