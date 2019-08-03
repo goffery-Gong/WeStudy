64afa8cd66503b4b223d1ba5929b7a6172f32b9b
goffery.gzw <goffery.gzw@alibaba-inc.com>
2019-08-03 10:58:15 +0800

    test2

---
+++ src/NetEase/gzw/QuestionOne.java
@@ -0,0 +1,13 @@
+package NetEase.gzw;
+
+import java.util.Scanner;
+
+/**
+ * @Author LingLong.gzw
+ * @create 2019-08-03
+ */
+public class QuestionOne {
+    public static void main(String[] args) {
+//        Scanner scanner=new Scanner();
+    }
+}
--- src/NetEase/gzw/QuestionTwo.java
+++ src/NetEase/gzw/QuestionTwo.java
@@ -1,11 +1,12 @@
-package NetEase;
+package NetEase.gzw;
 
 /**
  * @Author LingLong.gzw
  * @create 2019-08-03
  */
-public class QuestionOne {
+public class QuestionTwo {
     public static void main(String[] args) {
+        System.out.println();
         System.out.println(111);
     }
 }
