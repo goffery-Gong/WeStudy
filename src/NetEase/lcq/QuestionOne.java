726758e479cd5d3c96f06dbbc427ce5fc6eb6406
chuqi.lcq <chuqi.lcq@alibaba-inc.com>
2019-08-03 16:52:58 +0800

    tes1

--- src/NetEase/lcq/QuestionOne.java
+++ src/NetEase/lcq/QuestionOne.java
@@ -1,21 +1,68 @@
 package NetEase.lcq;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class QuestionOne {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        while(scanner.hasNext()){
-            int a = scanner.nextInt();
-            list.add(a);
-            int b = scanner.nextInt();
-            list.add(b);
+        int num = scanner.nextInt();
+        int[] score = new int[num];
+        for(int i=0;i<num;i++){
+            score[i] = scanner.nextInt();
         }
-        for(Integer in:list){
-            System.out.println("hhh"+in);
+        int queryNum =  scanner.nextInt();
+        int[] query = new int[queryNum];
+        for(int i=0;i<queryNum;i++){
+            query[i] = scanner.nextInt();
         }
+        for(int i = 0;i<queryNum;i++){
+            String result = calculate(score, queryNum, query[i]-1, num);
+            System.out.println(result);
+        }
+    }
+    public static String calculate(int[] score, int queryNum, int index, int num){
+        int s = score[index];
+        int count = 0;
+        int[] sortScore = new int[num];
+        sortScore = Arrays.copyOf(score, num);
+        Arrays.sort(sortScore);
+//        int lo = 0;
+//        int hi = num-1;
+//        int mid = lo+(hi-lo)/2;
+//        while(lo <= hi){
+//            mid = lo+(hi-lo)/2;
+//            if(sortScore[mid] > s){
+//                hi = mid-1;
+//            }else if(sortScore[mid] < s){
+//                lo = mid+1;
+//            }
+//            else{
+//                break;
+//            }
+//        }
+        if( s<= score[sortScore.length/2]){
+            for(int i=0;i<sortScore.length;i++){
+                if(sortScore[i] <= s) {
+                    count++;
+                }else{
+                    break;
+                }
+            }
+        }else{
+            for(int i=sortScore.length-1;i>=0;i--){
+                if(sortScore[i] > s) {
+                    count++;
+                }else{
+                    break;
+                }
+            }
+            count = num - count;
+        }
+        double res = (count-1)/(double)num;
+        //double res = (mid)/(double)num;
+        res = res*100;
+        String result = String.format("%.6f", res);
+        return result;
     }
 }
--- src/NetEase/lcq/QuestionTwo.java
+++ src/NetEase/lcq/QuestionTwo.java
@@ -1,7 +1,17 @@
 package NetEase.lcq;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class QuestionTwo {
     public static void main(String[] args) {
-        System.out.println(111);
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        int num = scanner.nextInt();
+        int[] array = new int[num];
+        for(int i=0;i<num;i++){
+            array[i] =
+        }
+
     }
 }
