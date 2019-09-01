124abfbcf0ed1570a42331c3958105dc402e3618
JCP@ZJU310 <jcp0578@gmail.com>
2019-09-01 21:53:57 +0800

    Q5 test

--- src/Tencent/JCP/Q5.java
+++ src/Tencent/JCP/Q5.java
@@ -16,18 +16,17 @@ public class Q5 {
 			b[i] = scanner.nextInt();
 			max_t = Math.max(max_t, b[i]);
 		}
-		BigInteger[] dp = new BigInteger[max_t + 1];
-		dp[0] = BigInteger.ONE;
+		long[] dp = new long[max_t + 1];
+		dp[0] = 1L;
 		for (int i = 1; i <= max_t; i++) {
-			dp[i] = BigInteger.ZERO;
 			if(i >= k)
-				dp[i] = dp[i].add(dp[i - k]);
-			dp[i] = dp[i].add(dp[i-1]);
+				dp[i] += dp[i - k];
+			dp[i] += dp[i-1];
 		}
 		for (int i = 0; i < t; i++) {
 			BigInteger out_t = BigInteger.ZERO;
 			for (int j = a[i]; j <= b[i]; j++)
-				out_t = out_t.add(dp[j]);
+				out_t = out_t.add(new BigInteger(dp[j]+""));
 			System.out.println(out_t);
 		}
 
