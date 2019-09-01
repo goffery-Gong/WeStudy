92db8a5e0d270503db8d272807177b974569d7cf
goffery.gzw <goffery.gzw@alibaba-inc.com>
2019-09-01 21:16:19 +0800

    [add]4

--- src/Tencent/gzw/Question4.java
+++ src/Tencent/gzw/Question4.java
@@ -3,6 +3,7 @@ package Tencent.gzw;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * @author goffery
@@ -19,10 +20,51 @@ public class Question4 {
         for (int i = 0; i < n; i++) {
             v[i].val = scanner.nextInt();
             inc[i] = i == 0 ? v[i].val : inc[i - 1] + v[i].val;
-            v[i].start=i;
-            v[i].end=i;
+            v[i].start = i;
+            v[i].end = i;
+        }
+        /*******************处理得到每个元素的左边界(正向遍历)*****************/
+        Stack<Integer> s = new Stack<>();
+        int i = 0;
+        while (i < (int)v.length) {
+            if (s.empty() || v[i].val > v[s.peek()].val) {
+                s.push(i);
+                ++i;
+            } else {
+                //插入的值更小的时候，将其start置为将要弹出的元素的start
+                v[i].start = v[s.peek()].start;
+                s.pop();
+            }
         }
 
+        /*******************处理得到每个元素的右边界(反向遍历)*****************/
+        while(!s.empty())//清空s,准备反向遍历
+            s.pop();
+        i=n-1;
+        while(i>=0){
+            if(s.empty() || v[i].val > v[s.peek()].val){
+                s.push(i);
+                --i;
+            }
+            else{
+                //插入的值更小的时候，将其start置为将要弹出的元素的start
+                v[i].end=v[s.peek()].end;
+                s.pop();
+            }
+        }
+        /******************得到每个元素的结果，返回最大值即可*****************/
+        long result=0;
+        int index_start=0,index_end=0;//得到最大值的区间(这里是从0开始计数的)
+        for(int k=0;k<n;++k){
+            long cur_result=v[k].val*(inc[v[k].end]-inc[v[k].start-1]);
+            if(cur_result>result){
+                result=cur_result;
+                index_start=v[k].start;
+                index_end=v[k].end;
+            }
+        }
+        System.out.println(result);
+        System.out.println(index_start+1 +" " +index_end+1);
     }
 
     class Node {
