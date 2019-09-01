908902de033c374df82dfca7231752249e390140
JCP@ZJU310 <jcp0578@gmail.com>
2019-09-01 21:19:13 +0800

    Q5

---
+++ src/Tencent/JCP/Q5.java
@@ -0,0 +1,31 @@
+package Tencent.JCP;
+
+import java.util.Scanner;
+
+public class Q5 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		int k = scanner.nextInt();
+		int[] a = new int[t];
+		int[] b = new int[t];
+		int max_t = 0;
+		for (int i = 0; i < t; i++) {
+			a[i] = scanner.nextInt();
+			b[i] = scanner.nextInt();
+			max_t = Math.max(max_t, b[i]);
+		}
+		int[] dp = new int[max_t + 1];
+		dp[0] = 1;
+		for (int i = 1; i <= max_t; i++) {
+			dp[i] = ((i >= k) ? dp[i - k] : 0) + dp[i - 1];
+		}
+		for (int i = 0; i < t; i++) {
+			int out_t = 0;
+			for (int j = a[i]; j <= b[i]; j++)
+				out_t += dp[j];
+			System.out.println(out_t);
+		}
+
+	}
+}
