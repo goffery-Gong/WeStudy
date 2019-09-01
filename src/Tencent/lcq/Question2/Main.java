6e9b2331a397b38dec4155304a9ef6dbefee7052
chuqi.lcq <chuqi.lcq@alibaba-inc.com>
2019-09-01 21:21:21 +0800

    Q4

--- src/Tencent/lcq/Question2/Main.java
+++ src/Tencent/lcq/Question2/Main.java
@@ -1,4 +1,15 @@
 package Tencent.lcq.Question2;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i=0;i<n;i++){
+            int a = scanner.nextInt();;
+            int b = scanner.nextInt();
+
+        }
+    }
 }
